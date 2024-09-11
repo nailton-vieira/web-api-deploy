83ce23c0ceff7e7d44cf2539c8a57703fc041d5a
nailton-vieira <nailtonvieira@gmail.com>
2024-09-11 10:49:43 -0300

    Atualizando webapi deploy

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3001
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.json({ message: "Servidor esta Rodando!" });
 }) 
 

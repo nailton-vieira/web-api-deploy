2400bb6599b97a68dbb75ee5c85e5d5a30c8d20d
nailton-vieira <nailtonvieira@gmail.com>
2024-09-10 21:27:15 -0300

    Adicionando base front e back ao repo

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3001
+/*
+app.get('/', (req, res) => {
+  res.send('Servidor Rodando')
+}) */
+
+  app.get("/api", (req, res) => {
+    res.json({ message: "Bem vindo ao Servidor!" });
+  });
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})

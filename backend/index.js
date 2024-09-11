4e453c5f3329979b29298c63ccbba40c0841bb27
nailton-vieira <nailtonvieira@gmail.com>
2024-09-10 23:16:11 -0300

    Atualizando vercel

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3001
-/*
+
 app.get('/', (req, res) => {
   res.send('Servidor Rodando')
-}) */
+}) 
 
   app.get("/api", (req, res) => {
     res.json({ message: "Bem vindo ao Servidor!" });

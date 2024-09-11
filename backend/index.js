cafe017cfa19c1d3d7655dfec9b0ecc14c485c94
nailton-vieira <nailtonvieira@gmail.com>
2024-09-10 23:41:24 -0300

    Atualizando vercel.js e app.js no  repo

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,6 @@ app.get('/', (req, res) => {
   res.json({ message: "Servidor esta Rodando!" });
 }) 
 
-  app.get("/api", (req, res) => {
-    res.json({ message: "Bem vindo ao Servidor!" });
-  });
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
